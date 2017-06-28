7f0b10a28737d28db3ae1138a9a5554f0e7c783f
LAPTOP-NRLTJDBF\Santhu <santoshbommakanti@my.unt.edu>
2017-06-27 23:56:09 -0500

    feat(module-api): Add Service Files to the Entity Vehicle

---
+++ api/src/main/java/io/egen/service/VehicleServiceImpl.java
@@ -0,0 +1,59 @@
+package io.egen.service;
+
+
+import io.egen.entity.Vehicle;
+import io.egen.exception.BadRequestException;
+import io.egen.exception.ResourceNotFoundException;
+import io.egen.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class VehicleServiceImpl implements VehicleService{
+
+    @Autowired
+    VehicleRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Vehicle> findAll() {
+        return repository.findAll();
+    }
+    @Transactional(readOnly = true)
+    public Vehicle findOne(String vin) {
+       Vehicle existing = repository.findOne(vin);
+        /*if (existing == null) {
+            throw new ResourceNotFoundException("Vehicle with vin " + vin + " doesn't exist.");
+        }*/
+        return existing;
+    }
+    @Transactional
+    public Vehicle create(Vehicle vehicle) {
+        Vehicle existing = repository.findByModel(vehicle.getModel());
+        if (existing != null) {
+            throw new BadRequestException("Vehicle with model " + vehicle.getModel() + " already exists.");
+        }
+        return repository.create(vehicle);
+    }
+    @Transactional
+    public Vehicle update(Vehicle vehicle) {
+        Vehicle existing = repository.findOne(vehicle.getVin());
+        if (existing == null) {
+            System.out.println(vehicle.getVin()+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            return repository.create(vehicle);
+        }
+        return repository.update(vehicle);
+    }
+    @Transactional
+    public void delete(String vin) {
+        Vehicle existing = repository.findOne(vin);
+        if (existing == null) {
+            throw new ResourceNotFoundException("Vehicle with vin " + vin + " doesn't exist.");
+        }
+       repository.delete(existing);
+
+    }
+	
+}
