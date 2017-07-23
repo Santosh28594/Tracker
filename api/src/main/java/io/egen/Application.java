f7990244164e9e4c35d019870923040a2110e817
LAPTOP-NRLTJDBF\Santhu <santoshbommakanti@my.unt.edu>
2017-07-23 01:30:52 -0500

    refactor(module-api): Adjust code to support jdbc template

--- api/src/main/java/io/egen/Application.java
+++ api/src/main/java/io/egen/Application.java
@@ -1,10 +1,21 @@
 package io.egen;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import io.egen.repository.AlertCountRepository;
+import io.egen.repository.AlertCountRepositoryImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import javax.sql.DataSource;
+
 
 @Configuration
 @ComponentScan
@@ -20,4 +31,34 @@ public class Application extends WebMvcConfigurerAdapter {
                 .exposedHeaders("header1", "header2")
                 .allowCredentials(false).maxAge(3600);
     }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //MySQL database we are using
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/veh_db");//change url
+        dataSource.setUsername("root");//change userid
+        dataSource.setPassword("root");//change pwd
+
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public AlertCountRepository alertCountRepository(){
+       AlertCountRepository alertCountRepo = new AlertCountRepositoryImpl();
+        alertCountRepo.setJdbcTemplate(jdbcTemplate());
+        return alertCountRepo;
+    }
+
+
+
 }
